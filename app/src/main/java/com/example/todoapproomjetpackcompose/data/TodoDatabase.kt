0581350166e8aa1f87cda47197b1fc84aadc764c
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-24 09:24:35 +0900

    Update TodoDB

--- app/src/main/java/com/example/todoapproomjetpackcompose/data/TodoDatabase.kt
+++ app/src/main/java/com/example/todoapproomjetpackcompose/data/TodoDatabase.kt
@@ -1,2 +1,28 @@
 package com.example.todoapproomjetpackcompose.data
 
+import android.app.Application
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Todo::class], version = 1)
+abstract class TodoDatabase(): RoomDatabase() {
+    abstract fun todoDao(): TodoDao
+
+    companion object {
+        var INSTANSE: TodoDatabase? = null
+
+        fun createDb(context: Context): TodoDatabase {
+            val instanse = INSTANSE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    TodoDatabase::class.java,
+                    "todo_database"
+                ).build()
+            }
+            INSTANSE = instanse
+            return instanse
+        }
+    }
+}
