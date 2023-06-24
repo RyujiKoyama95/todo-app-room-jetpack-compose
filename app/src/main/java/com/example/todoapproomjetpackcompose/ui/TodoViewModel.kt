ec3970f2c28bc18abc3bd1249b891a9543d5c543
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-24 11:36:53 +0900

    Add factory to todo viewmodel

--- app/src/main/java/com/example/todoapproomjetpackcompose/ui/TodoViewModel.kt
+++ app/src/main/java/com/example/todoapproomjetpackcompose/ui/TodoViewModel.kt
@@ -1,7 +1,11 @@
-package com.example.todoapproomjetpackcompose.ui.theme
+package com.example.todoapproomjetpackcompose.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.todoapproomjetpackcompose.data.Todo
 import com.example.todoapproomjetpackcompose.data.TodoRepository
 import com.example.todoapproomjetpackcompose.data.TodoStatus
@@ -10,9 +14,23 @@ import kotlinx.coroutines.launch
 class TodoViewModel(
     private val repository: TodoRepository
     ): ViewModel() {
+    companion object {
+        const val TAG = "TodoViewModel"
+        val factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras
+            ): T {
+                val context = checkNotNull(extras[APPLICATION_KEY])
+                val repository = TodoRepository(context)
+                return TodoViewModel(repository) as T
+            }
+        }
+    }
 
     // dummy data insert
     init {
+        Log.d(TAG, "init")
         val dummyData = Todo(1,"dummy", TodoStatus.NOT_COMPLETED)
         createTodo(dummyData)
     }
@@ -22,7 +40,9 @@ class TodoViewModel(
     fun getTodoList() {
         viewModelScope.launch {
             todoList = repository.getTodoList()
+            Log.d(TAG, "1 $todoList")
         }
+        Log.d(TAG, "2 $todoList")
     }
 
     fun createTodo(todo: Todo) {
