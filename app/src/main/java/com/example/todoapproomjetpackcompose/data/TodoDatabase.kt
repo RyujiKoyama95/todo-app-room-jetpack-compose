7760a3a6486361651b2dc02e6265fd99cfa5437e
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-26 22:07:00 +0900

    Fix code

--- app/src/main/java/com/example/todoapproomjetpackcompose/data/TodoDatabase.kt
+++ app/src/main/java/com/example/todoapproomjetpackcompose/data/TodoDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Todo::class], version = 1)
+@Database(entities = [Todo::class], version = 1, exportSchema = false)
 abstract class TodoDatabase(): RoomDatabase() {
     abstract fun todoDao(): TodoDao
 
@@ -21,7 +21,7 @@ abstract class TodoDatabase(): RoomDatabase() {
                     context,
                     TodoDatabase::class.java,
                     "todo_database"
-                ).fallbackToDestructiveMigration().build()
+                ).build()
             }
             INSTANCE = instance
             return instance
