dd6515ee5cfe9052dcc4db73eeacca05fdbeb243
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-26 21:37:29 +0900

    Add workaround

--- app/src/main/java/com/example/todoapproomjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/todoapproomjetpackcompose/MainActivity.kt
@@ -12,15 +12,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.todoapproomjetpackcompose.data.Todo
+import com.example.todoapproomjetpackcompose.data.TodoDao
+import com.example.todoapproomjetpackcompose.data.TodoDatabase
+import com.example.todoapproomjetpackcompose.data.TodoRepository
+import com.example.todoapproomjetpackcompose.data.TodoStatus
 import com.example.todoapproomjetpackcompose.ui.TodoViewModel
 import com.example.todoapproomjetpackcompose.ui.theme.TodoAppRoomJetpackComposeTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     companion object {
         const val TAG = "MainActivity"
     }
 
-    val viewModel: TodoViewModel by viewModels {TodoViewModel.factory}
+//    val viewModel: TodoViewModel by viewModels {TodoViewModel.factory}
+    lateinit var dao: TodoDao
+    lateinit var todoList: MutableList<Todo>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +43,49 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Log.d(TAG, "onCreate")
-                    viewModel.getTodoList()
-                    Log.d(TAG, "step1")
-                    val text = viewModel.todoList[0].title
+                    dao = TodoDatabase.createDb(applicationContext).todoDao()
+                    todoList = mutableListOf<Todo>()
+
+                    val dummyData = Todo(1,"dummy", TodoStatus.NOT_COMPLETED)
+                    createTodo(dummyData)
+                    Thread.sleep(5000)
+
+                    getTodoList()
+                    Thread.sleep(5000)
+                    val text = todoList[0].title
+
                     Greeting(text)
                 }
+            }
+        }
+    }
+    private fun getTodoList() {
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.Default) {
+                Log.d(TAG, "getTodoList start")
+                dao.observeAll().forEach {
+                    todoList.add(it)
+                }
+                Log.d(TAG, "getTodoList end")
+            }
+        }
+    }
+
+    private fun createTodo(todo: Todo) {
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.Default) {
+                Log.d(TAG, "createTodo start")
+                // Todo: daoのAPIが呼べていなさそう
+                dao.insert(todo)
+                Log.d(TAG, "createTodo end")
+            }
+        }
+    }
+
+    private fun delete(todo: Todo) {
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.Default) {
+                dao.delete(todo)
             }
         }
     }
