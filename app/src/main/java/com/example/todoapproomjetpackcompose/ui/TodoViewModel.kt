526924f205e65e038c473a1dd75f74178baf59c1
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-30 22:46:23 +0900

    Add view model factory

--- app/src/main/java/com/example/todoapproomjetpackcompose/ui/TodoViewModel.kt
+++ app/src/main/java/com/example/todoapproomjetpackcompose/ui/TodoViewModel.kt
@@ -43,7 +43,8 @@ class TodoViewModel(
 
     fun updateTodo(todo: Todo) {
         viewModelScope.launch {
-            repository.updateTodo(todo )
+            repository.updateTodo(todo)
+            getTodoList()
         }
     }
 
