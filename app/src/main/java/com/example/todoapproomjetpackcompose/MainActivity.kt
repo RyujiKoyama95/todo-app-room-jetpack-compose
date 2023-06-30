47347250c29244332cde88f396601293bec90a0c
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-06-30 23:01:09 +0900

    Remove debug code

--- app/src/main/java/com/example/todoapproomjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/todoapproomjetpackcompose/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,53 +27,8 @@ class MainActivity : ComponentActivity() {
         const val TAG = "MainActivity"
     }
 
-    private val viewModel: TodoViewModel by viewModels {TodoViewModel.factory}
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         Log.d(TAG, "onCreate")
-
-        val dummyData = Todo(title = "dummy data", status = TodoStatus.NOT_COMPLETED)
-        viewModel.createTodo(dummyData)
-        Log.d(TAG, "onCreate todoList=${viewModel.todos}")
-
-        setContent {
-            TodoAppRoomJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val todoList by viewModel.todos.observeAsState(emptyList())
-                    Greeting(showTodoTitle(todoList))
-                }
-            }
-        }
-    }
-
-    private fun showTodoTitle(todoList: List<Todo>): String {
-        var title = ""
-        if(todoList.isNotEmpty()) {
-            Log.d(TAG,"showTodoTitle todoList=$todoList")
-            title = todoList[0].title
-        }
-        return title
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoAppRoomJetpackComposeTheme {
-        Greeting("Android")
     }
 }
