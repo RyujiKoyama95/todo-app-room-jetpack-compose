765d62dd0e871776c513e7fdc4689ce0d225af08
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-01 14:32:03 +0900

    Update process for connecting viewmodel to view

--- app/src/main/java/com/example/todoapproomjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/todoapproomjetpackcompose/MainActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -18,18 +16,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -76,7 +73,9 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainContent(viewModel: TodoViewModel) {
-    val isShowDialog: MutableState<Boolean> = remember { mutableStateOf(false) }
+    val isShowDialog = remember { mutableStateOf(false) }
+    val title = remember { mutableStateOf("") }
+    val description = remember { mutableStateOf("") }
 
     Scaffold(floatingActionButton =  {
         FloatingActionButton(onClick = { isShowDialog.value = true }) {
@@ -86,6 +85,17 @@ fun MainContent(viewModel: TodoViewModel) {
         if (isShowDialog.value) {
             EditDialog(isShowDialog = isShowDialog, viewModel = viewModel)
         }
+
+        val todos = viewModel.todos.value
+        if (todos != null) {
+            if (todos.isNotEmpty()) {
+                title.value = todos[0].title
+                description.value = todos[0].description
+            }
+        }
+
+        Text(text = title.value)
+        Text(text = description.value)
     }
 }
 
@@ -94,21 +104,27 @@ fun MainContent(viewModel: TodoViewModel) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditDialog(isShowDialog: MutableState<Boolean>, viewModel: TodoViewModel) {
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
     AlertDialog(
         onDismissRequest = { isShowDialog.value = false },
         title = { Text(text = "create Todo") },
         text = {
             Column {
                 Text(text = "title")
-                TextField(value = "", onValueChange = { /* Todo */ })
+                OutlinedTextField(value = title, onValueChange = { title = it })
                 Text(text = "description")
-                TextField(value = "", onValueChange = { /* Todo */ })
+                OutlinedTextField(value = description, onValueChange = { description = it })
             }
         },
         confirmButton = {
             Button(onClick = { isShowDialog.value = false }) {
                 Text(text = "OK")
             }
+            viewModel.createTodo(
+                Todo(title = title, description = description, status = TodoStatus.NOT_COMPLETED)
+            )
         },
         dismissButton =  {
             Button(onClick = { isShowDialog.value = false }) {
--- app/src/main/java/com/example/todoapproomjetpackcompose/data/Todo.kt
+++ app/src/main/java/com/example/todoapproomjetpackcompose/data/Todo.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 data class Todo(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
+    val description: String,
     val status: TodoStatus
 )
 
