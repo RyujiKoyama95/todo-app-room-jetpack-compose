2ae5e1c436d2a31e6d773488d7e9760281d542f8
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-07-02 21:20:07 +0900

    Fix list component

--- app/src/main/java/com/example/todoapproomjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/todoapproomjetpackcompose/MainActivity.kt
@@ -15,11 +15,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -34,6 +37,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
@@ -163,7 +167,19 @@ fun TodoList(viewModel: TodoViewModel) {
     observedTodos.value?.let { todos ->
         LazyColumn {
             items(todos) {
-                Text(text = it.title)
+                TodoRow(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoRow(todo: Todo) {
+    Card() {
+        Row() {
+            Text(text = todo.title)
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
             }
         }
     }
